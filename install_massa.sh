16fdef912452ecc7a797738e11dbd3ac5d3b9f9a
Thybault Alabarbe <thybault.alabarbe@gmail.com>
2024-12-25 22:15:11 +0100

    install proot dependencies automatically

--- install_massa.sh
+++ install_massa.sh
@@ -1,17 +1,42 @@
 #!/bin/bash
 
-set -uo pipefail
+set -euo pipefail
+
+scan_installed_dependencies() {
+  declare -a -g missing_termux_deps=()
+  declare -g has_proot_distro=false
+  declare -g has_node=false
+  declare -g has_deweb=false
+  if ! pkg list-installed proot-distro 2>/dev/null | grep -q "proot-distro"; then
+    missing_termux_deps+=("proot-distro")
+  fi
+  if [ -d "$proot_distro_rootfs" ]; then
+    has_proot_distro=true
+  fi
+  if [ -d "$node_path" ]; then
+    has_node=true
+  fi
+  if [ -d "$deweb_path" ]; then
+    has_deweb=true
+  fi
+}
+
+run_in_proot_distro() {
+  proot-distro login "$proot_distro_rootfs" -- bash -c "${@@Q}"
+}
 
 log() {
-  printf "%s\n" "$@" >> "$logfile"
+  printf "%s\n" "$@" >> "$logtmp"
+  if ! "$alt_screen"; then
+    printf "%s\n" "$@"
+  fi
 }
 
 # Initialize the terminal to use the alternate screen
 init_altscreen() {
-  data="$(mktemp -d)"
-  logfile="$data/log"
-  touch "$logfile"
-  log "init_altscreen"
+  set +e
+  [ ! -f "$logfile" ] && touch "$logfile"
+  [ ! -f "$logtmp" ] && touch "$logtmp"
   # switch to the alternate screen
   declare -g _stty
   _stty="$(stty -g < /dev/tty)"
@@ -19,22 +44,25 @@ init_altscreen() {
   tput smcup
   # hide the cursor
   tput civis
+  alt_screen=true
 }
 
 drop_altscreen() {
-  log "drop_altscreen"
   stty "$_stty" < /dev/tty
   # switch back to the normal screen
   tput rmcup
   # show the cursor
   tput cnorm
+  set -e
   echo "Logs:"
-  cat "$logfile"
-  rm -rf "$data"
+  cat "$logtmp"
+  printf "[%s]\n" "$start_time" >> "$logfile"
+  cat "$logtmp" >> "$logfile"
+  rm "$logtmp"
+  alt_screen=false
 }
 
 waitrawkey() {
-  log "waitrawkey"
   keyvar=${1:-menu_key}
   acc=""
   while true; do
@@ -51,11 +79,9 @@ waitrawkey() {
     fi
   done
   declare -g "$keyvar=$acc"
-  log "key: ${acc@Q}"
 }
 
 waitkey() {
-  log "waitkey"
   waitrawkey key
   case $key in
     $'\E[A') key="UP" ;;
@@ -63,17 +89,14 @@ waitkey() {
     $'\E') key="ESC" ;;
     $'\n') key="ENTER" ;;
   esac
-  log "key: $key"
 }
 
 get_cursor_pos() {
-  log "get_cursor_pos"
   IFS=';' read -sdrR -p $'\E[6n' ROW COL
   echo "${ROW#*[}" "${COL}"
 }
 
 draw_line() {
-  log "draw_line $*"
   if [ "$1" -lt 0 ]; then
     local row="$(($(tput lines) + $1))"
     tput cup "$row" 0
@@ -84,13 +107,11 @@ draw_line() {
 }
 
 resize_menu() {
-  log "resize_menu"
   clear
   menu_loop
 }
 
 run_menu() {
-  log "run_menu"
   clear
   init_menu
   menu_loop
@@ -98,12 +119,28 @@ run_menu() {
 
 shadow=$'\033[1;30m'
 reset=$'\033[0m'
+
+make_styles() {
+  size="$1"
+  declare -g -a style=("")
+  for _ in $(seq 1 "$size"); do
+    style+=("$shadow")
+  done
+  style["$pos"]=""
+}
+
+make_boxes() {
+  size="$1"
+  declare -g -a boxes=()
+  for _ in $(seq 1 "$size"); do
+    boxes+=(" ")
+  done
+}
+
 init_menu() {
-  log "init_menu"
   declare -g pos=0
-  declare -g -a style=("" "$shadow" "$shadow" "$shadow" "$shadow" "$shadow")
-  declare -g -a boxes=(" " " " " " " " " " " ")
-  declare -g prev_pos=0
+  make_styles 6
+  make_boxes 6
   draw_line 0 "Welcome to the massa android installer!"
   draw_line 1 "What you would like to do?"
   draw_line -4 "↑/↓: move"
@@ -113,23 +150,24 @@ init_menu() {
 }
 
 menu_loop() {
-  log "menu_loop"
   while true; do 
     draw_line 3  "${style[0]} [${boxes[0]}] Install: [De]Select all$reset"
     draw_line 4  "${style[1]}   [${boxes[1]}] node$reset"
     draw_line 5  "${style[2]}   [${boxes[2]}] deweb client$reset"
     draw_line 6  "${style[3]} [${boxes[3]}] Uninstall: [De]Select all$reset"
     draw_line 7  "${style[4]}   [${boxes[4]}] Uninstall the node$reset"
-    draw_line 8 "${style[5]}   [${boxes[5]}] Uninstall the deweb client$reset"
+    draw_line 8  "${style[5]}   [${boxes[5]}] Uninstall the deweb client$reset"
     waitkey
     case $key in
       "UP")
-        prev_pos=$pos;
+        style["$pos"]="$shadow"
         pos=$((pos>0?pos-1:0))
+        style["$pos"]=""
         ;;
       "DOWN")
-        prev_pos=$pos;
+        style["$pos"]="$shadow"
         pos=$((pos<5?pos+1:5))
+        style["$pos"]=""
         ;;
       " ") 
         if [ "${boxes[$pos]}" = " " ]; then
@@ -162,7 +200,6 @@ menu_loop() {
         else
           boxes["$pos"]="$box"
           sibling=$(((pos+3) % 6))
-          log "pos: $pos, sibling: $sibling"
           if [ "$box" = "X" ]; then
             boxes["$sibling"]=" "
           fi
@@ -181,13 +218,8 @@ menu_loop() {
       "ENTER") break ;;
       "ESC"|"q") exit ;;
     esac
-    if [ "$prev_pos" -ne "$pos" ]; then
-      style["$prev_pos"]="$shadow"
-      style["$pos"]=""
-    fi
   done
-  log "selected:"
-  on_exit
+  drop_altscreen
   [ "${boxes[1]}" = "X" ] && install_node
   [ "${boxes[2]}" = "X" ] && install_deweb
   [ "${boxes[4]}" = "X" ] && uninstall_node
@@ -195,31 +227,71 @@ menu_loop() {
 }
 
 on_exit() {
-  log "on_exit"
   drop_altscreen
   trap - EXIT
 }
 
-on_start() {
+start() {
   trap on_exit EXIT
   init_altscreen
   run_menu
 }
 
+install_proot_distro_deps() {
+  log "installing proot-distro dependencies"
+  pkg update
+  pkg install "${missing_termux_deps[@]@Q}"
+  missing_termux_deps=()
+}
+
+install_proot_distro() {
+  log "installing proot-distro"
+  log "${#missing_termux_deps[@]}"
+  if [ "${#missing_termux_deps[@]}" -gt 0 ]; then
+    install_proot_distro_deps
+  fi
+  log "checking proot-distro vm"
+  log "$has_proot_distro"
+  if ! $has_proot_distro; then
+    log "installing proot-distro vm"
+    proot-distro install --override-alias droid.massa ubuntu-oldlts
+    log "updating proot-distro vm"
+    run_in_proot_distro "apt update && apt upgrade -y"
+  fi
+}
+
 install_node() {
-  echo "installing node"
+  log "installing node"
+  install_proot_distro
 }
 
 uninstall_node() {
-  echo "uninstalling node"
+  log "uninstalling node"
 }
 
 install_deweb() {
-  echo "installing deweb"
+  log "installing deweb"
 }
 
 uninstall_deweb() {
-  echo "uninstalling deweb"
+  log "uninstalling deweb"
 }
 
-on_start
+data="$HOME/.config/droid.massa"
+
+logfile="$data/log"
+start_time="$(date)"
+logtmp="$data/log$(date +%s)"
+
+[ ! -d "$data" ] && mkdir -p "$data"
+vm_name="droid.massa"
+proot_distro_rootfs="$PREFIX/var/lib/proot-distro/installed-rootfs/$vm_name"
+proot_distro_shared="$HOME/.virtual/proot-distro/$vm_name"
+[ ! -d "$proot_distro_shared" ] && mkdir -p "$proot_distro_shared"
+node_path="$proot_distro_shared/node"
+deweb_path="$proot_distro_shared/deweb"
+declare -g alt_screen=false
+
+scan_installed_dependencies
+
+start
